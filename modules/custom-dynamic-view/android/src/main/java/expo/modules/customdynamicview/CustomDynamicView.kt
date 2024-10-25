78f6165eed60487926627da9554d5a0dbbdde120
MrFaraday <faradayby@gmail.com>
2024-10-25 12:12:04 +0300

    minors

--- modules/custom-dynamic-view/android/src/main/java/expo/modules/customdynamicview/CustomDynamicView.kt
+++ modules/custom-dynamic-view/android/src/main/java/expo/modules/customdynamicview/CustomDynamicView.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.util.Log
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import expo.modules.kotlin.AppContext
 import expo.modules.kotlin.views.ExpoView
@@ -14,18 +15,45 @@ class CustomDynamicView(context: Context, appContext: AppContext) : ExpoView(con
 
     init {
         root = LinearLayout(context)
+        root.orientation = VERTICAL
+//        root.layoutParams =
+//            ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
         root.layoutParams =
-            ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
+            ViewGroup.LayoutParams(LayoutParams.MATCH_PARENT, 500)
         addView(root)
+
+        
+
+        val button = Button(context)
+        button.layoutParams = ViewGroup.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT)
+        button.setOnClickListener {
+            addCustomView()
+        }
+
+        button.text = "adsdfasdf"
+
+        root.addView(button)
     }
 
     fun addCustomView() {
         this.post {
-            val view = View(context)
-            view.layoutParams = ViewGroup.LayoutParams(400, 400)
-            view.setBackgroundColor(Color.RED)
+            val view = CustomView(context)
             root.addView(view)
-//            Log.d("cmodule", "view added")
+        }
+    }
+}
+
+class CustomView(context: Context) : View(context) {
+    init {
+        this.layoutParams = ViewGroup.LayoutParams(200, 200)
+        this.setBackgroundColor(Color.RED)
+
+        this.post {
+            this.invalidate()
+            this.requestLayout()
+            this.forceLayout()
+            this.layoutParams.width = 300
+            this.layoutParams.height = 300
         }
     }
 }
